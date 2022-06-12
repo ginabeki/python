bdeb4776c533376f9c1ae4be0c39dc12966da355
ginabeki <ginabeki3@gmail.com>
2022-06-12 22:00:32 +0300

    practicee

--- WebDevelopment/website/__init__.py
+++ WebDevelopment/website/__init__.py
@@ -1,12 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+# setting up database
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hdhhsdywdfeirrrrehdf'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # initialise database
+    db.init_app(app)
+    
     from.views import views
     from.auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from.models import User, Note
+    create_database(app)
     return app
+# create database
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
